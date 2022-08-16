adc74ba6b08b860c36286ff8339d7bfaeeb36698
Kristina <ya.kep0602@yandex.ru>
2022-08-16 23:14:59 +0300

    Решение задачи №29

---
+++ task29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+ и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] 
+*/
+
+
+int [] array = {1,2,3,4,5,17,44,23}; 
+
+void PrintArray(int[] array) 
+{ 
+int count = array.Length; 
+
+for (int i = 0; i < count; i++) 
+{
+    Console.Write($"{array[i]} ");
+} 
+}
+
+Console.WriteLine($"Массив: [ {String.Join("; ",array)} ]");
