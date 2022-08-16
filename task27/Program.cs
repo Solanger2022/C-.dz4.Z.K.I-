b0d48fdac4fc5b53b8ae5ae69ba6dbbcbfaef1b6
Kristina <ya.kep0602@yandex.ru>
2022-08-16 23:18:03 +0300

    Решение задачи №27 не считает.

---
+++ task27/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */ 
+
+Console.Write("Введите число: "); 
+
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int SumNumbers ( int number) 
+{ 
+int result = 0; 
+
+    while (number > 0)
+    {
+        result = result + number % 10; 
+        number = number / 10; 
+
+    } 
+
+return result; 
+
+} 
+Console.WriteLine($"Суммa цифр в числе {SumNumbers}");
+
+
